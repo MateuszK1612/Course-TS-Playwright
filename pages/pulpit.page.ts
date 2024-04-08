da64b601d300bfc83c2d409197644ebe3982eb1c
Mateusz161994 <kontakt.mkowalczyk@gmail.com>
2024-04-08 12:51:41 +0200

    adding components

--- pages/pulpit.page.ts
+++ pages/pulpit.page.ts
@@ -1,17 +1,21 @@
 import { Page } from '@playwright/test';
+import { SideMenuComponent } from '../components/side-menu.component';
 
 export class PulpitPage {
   constructor(private page: Page) {}
+
+  sideMenu = new SideMenuComponent(this.page)
+
   transferReceiver = this.page.locator('#widget_1_transfer_receiver');
   transferAmount = this.page.locator('#widget_1_transfer_amount');
   transferTitle = this.page.locator('#widget_1_transfer_title');
   transferButton = this.page.getByRole('button', { name: 'wykonaj' });
   closeButton = this.page.getByTestId('close-button');
-  showMessage = this.page.locator('#show_messages')
+  showMessage = this.page.locator('#show_messages');
 
   topupReceiver = this.page.locator('#widget_1_topup_receiver');
   topupAmount = this.page.locator('#widget_1_topup_amount');
   topupAgreement = this.page.locator('#uniform-widget_1_topup_agreement span');
   topupButton = this.page.getByRole('button', { name: 'doładuj telefon' });
-  moneyValue = this.page.locator('#money_value')
+  moneyValue = this.page.locator('#money_value');
 }
